ff2027d1f725acd6b1ec10cf24da83d8c5ca40bc
halabikeren <halabikeren@gmail.com>
2020-05-19 10:43:26 +0300

    Added frequency count across samples for the metadata field grouped by

--- python/covid19/process_variation_data.py
+++ python/covid19/process_variation_data.py
@@ -16,11 +16,12 @@ if __name__ == '__main__':
 
     # read input file
     vcf_reader = vcf.Reader(open(input_path))
+    records = [record for record in vcf_reader]
 
     # collect the SNPs and their frequencies across all the samples
     SNPs_to_frequency = dict()
     total_count = 0
-    for record in vcf_reader:
+    for record in records:
         total_count += 1
         if (str(record.CHROM), str(record.POS), str(record.REF), str(record.ALT)) not in SNPs_to_frequency:
             SNPs_to_frequency[(str(record.CHROM), str(record.POS), str(record.REF), str(record.ALT))] = 1
@@ -39,8 +40,8 @@ if __name__ == '__main__':
     metadata_to_group_by_regex = re.compile('_' + metadata_to_group_by + '_(.*?)[_|;]', re.DOTALL)
     data = dict()
     metadata_to_samples_count = dict()
-    for record in vcf_reader:
-        metadata_value = metadata_to_group_by_regex.search(record.INFO)
+    for record in records:
+        metadata_value = metadata_to_group_by_regex.search(record.INFO['QNAME']).group(1)
         if not metadata_value in metadata_to_samples_count:
             metadata_to_samples_count[metadata_value] = 1
         else:
@@ -56,11 +57,14 @@ if __name__ == '__main__':
             data[metadata_value][SNP] /= metadata_to_samples_count[metadata_value]
 
     # insert data to a dataframe
-    df = pd.DataFrame(columns=[metadata_to_group_by] + [
+    total_samples_count = sum(metadata_to_samples_count[metadata_value] for metadata_value in metadata_to_samples_count)
+    df = pd.DataFrame(columns=[metadata_to_group_by, 'frequency_across_samples'] + [
         'chrom_' + str(frequent_SNP[0]) + '_pos_' + frequent_SNP[1] + '_ref_' + frequent_SNP[2] + '_alt_' + frequent_SNP[3] for frequent_SNP in frequent_SNPs])
     for metadata_value in metadata_to_samples_count:
         value = {metadata_to_group_by: metadata_value}
+        value['frequency_across_samples'] = metadata_to_samples_count[metadata_value] / total_samples_count
         for SNP in frequent_SNPs:
             value['chrom_' + SNP[0] + '_pos_' + SNP[1] + '_ref_' + SNP[2] + '_alt_' + SNP[3]] = data[metadata_value][SNP]
+        df = df.append(value, ignore_index = True)
     df.to_csv(output_path)
 
